689607a2f2f3b2e03d70bbeddb1115178ffa298a
Павел Храпкин <Khrapkin.P@csitltd.ru>
2018-04-14 19:54:54 +0300

    Lesson 1 - Create Beam. I did it in WPF

--- Lesson_1_CreateBeam/MainWindow.xaml.cs
+++ Lesson_1_CreateBeam/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tekla.Structures;
+using Tekla.Structures.Model;
+using T3D = Tekla.Structures.Geometry3d;
 
 namespace Lesson_1_CreateBeam
 {
@@ -23,6 +26,19 @@ namespace Lesson_1_CreateBeam
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Model Model = new Model();
+            Beam NewBeam = new Beam();
+            NewBeam.StartPoint = new T3D.Point(0, 0, 0);
+            NewBeam.EndPoint = new T3D.Point(1000, 0, 0);
+            NewBeam.Profile.ProfileString = "I40B1_20_93";
+            NewBeam.Material.MaterialString = "C245";
+            NewBeam.Class = "4";
+            NewBeam.Insert();
+            Model.CommitChanges();
         }
     }
 }
